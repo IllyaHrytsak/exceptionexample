eb08055e127fa6aaa33b6b99dc1f5d1eb360d83e
IllyaHrytsak <gritsak_web@bigmir.net>
2017-04-20 23:33:03 +0300

    Added new constants

--- src/main/java/ua/training/view/StringConstants.java
+++ src/main/java/ua/training/view/StringConstants.java
@@ -4,18 +4,20 @@ package ua.training.view;
 public interface StringConstants {
 
     //Input data
-    String INPUT_NAME = "Input your name: ";
-    String INPUT_SURNAME = "Input your surname: ";
-    String INPUT_PATRONYMIC = "Input your patronymic: ";
-    String INPUT_HOME_NUMBER = "Input your home number: ";
-    String INPUT_MOBILE_NUMBER = "Input your mobile number: ";
-    String INPUT_EMAIL = "Input your email: ";
-    String INPUT_SKYPE = "Input your skype: ";
-    String INPUT_INDEX = "Input your index: ";
-    String INPUT_TOWN = "Input your town: ";
-    String INPUT_STREET = "Input your street: ";
-    String INPUT_HOUSE_NUMBER = "Input your house number: ";
-    String INPUT_APARTMENT_NUMBER = "Input your apartment number: ";
+    String INPUT_NAME = "Input your name(e.g. 'Vasya'): ";
+    String INPUT_SURNAME = "Input your surname(e.g. 'Pupkin'): ";
+    String INPUT_PATRONYMIC = "Input your patronymic(e.g. 'Andreevich'): ";
+    String INPUT_NICKNAME = "Input your nickname(e.g. 'KlliK'): ";
+    String INPUT_HOME_NUMBER = "Input your home number(e.g. +38(044)5608260): ";
+    String INPUT_MOBILE_NUMBER1 = "Input your mobile number(e.g. +38(093)5215824): ";
+    String INPUT_MOBILE_NUMBER2 = "Input your second mobile number(write 'next' to skip): ";
+    String INPUT_EMAIL = "Input your email(e.g. illyah@gmail.com): ";
+    String INPUT_SKYPE = "Input your skype(e.g. illa_gritsak): ";
+    String INPUT_INDEX = "Input your index(e.g. 02091): ";
+    String INPUT_TOWN = "Input your town(e.g. Milan): ";
+    String INPUT_STREET = "Input your street(e.g. Dekabristov): ";
+    String INPUT_HOUSE_NUMBER = "Input your house number(e.g. 10a): ";
+    String INPUT_APARTMENT_NUMBER = "Input your apartment number(e.g. 101): ";
 
     //Wrong input data
     String WRONG_INPUT_DATA = "Wrong input data";
