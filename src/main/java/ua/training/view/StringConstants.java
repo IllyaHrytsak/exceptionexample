c275d6f09c6805e0f101daf086f3e35e87395c7d
Illya Hrytsak <gritsak_web@bigmir.net>
2017-04-20 16:53:57 +0300

    StringConstants was created

---
+++ src/main/java/ua/training/view/StringConstants.java
@@ -0,0 +1,25 @@
+package ua.training.view;
+
+
+public interface StringConstants {
+
+    //Input data
+    String INPUT_NAME = "Input your name: ";
+    String INPUT_SURNAME = "Input your surname: ";
+    String INPUT_PATRONYMIC = "Input your patronymic: ";
+    String INPUT_HOME_NUMBER = "Input your home number: ";
+    String INPUT_MOBILE_NUMBER = "Input your mobile number: ";
+    String INPUT_EMAIL = "Input your email: ";
+    String INPUT_SKYPE = "Input your skype: ";
+    String INPUT_INDEX = "Input your index: ";
+    String INPUT_TOWN = "Input your town: ";
+    String INPUT_STREET = "Input your street: ";
+    String INPUT_HOUSE_NUMBER = "Input your house number: ";
+    String INPUT_APARTMENT_NUMBER = "Input your apartment number: ";
+
+    //Wrong input data
+    String WRONG_INPUT_DATA = "Wrong input data";
+
+
+
+}
