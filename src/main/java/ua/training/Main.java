8ab9fe2511db8f0f3ba8c24a49a046d624f4bb56
Illya Hrytsak <gritsak_web@bigmir.net>
2017-04-20 16:54:21 +0300

    Main was created

---
+++ src/main/java/ua/training/Main.java
@@ -0,0 +1,25 @@
+package ua.training;
+
+
+import ua.training.controller.Controller;
+import ua.training.model.Model;
+import ua.training.view.View;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args ) {
+        Model model = new Model();
+        View view = new View();
+
+        Controller controller = new Controller(view, model);
+
+        controller.processUser();
+
+
+    }
+}
