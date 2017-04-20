5809f45a8e9a2e771a4b0c9383139dd598ee62d9
IllyaHrytsak <gritsak_web@bigmir.net>
2017-04-20 23:32:37 +0300

    Optimization

--- src/main/java/ua/training/controller/UtilityController.java
+++ src/main/java/ua/training/controller/UtilityController.java
@@ -7,16 +7,18 @@ import java.util.Scanner;
 public class UtilityController {
 
     private View view;
+    private Scanner scanner;
 
-    public UtilityController(View view) {
+    public UtilityController(View view, Scanner scanner) {
+        this.scanner = scanner;
         this.view = view;
     }
 
-    public String inputValueStringWithRegex(Scanner sc, String regex) {
-        String value = sc.next();
+    public String inputValueStringWithRegex(String regex) {
+        String value = scanner.next();
         while(!(value.matches(regex))) {
             view.showWrongInput();
-            value = sc.next();
+            value = scanner.next();
         }
 
         return value;
