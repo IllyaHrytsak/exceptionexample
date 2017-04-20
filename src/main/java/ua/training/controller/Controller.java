c77d643f6d925cb67d2b4c095a9203bc7c63ee70
Illya Hrytsak <gritsak_web@bigmir.net>
2017-04-20 16:55:28 +0300

    Controller was created

---
+++ src/main/java/ua/training/controller/Controller.java
@@ -0,0 +1,26 @@
+package ua.training.controller;
+
+
+import ua.training.model.Model;
+import ua.training.view.View;
+
+import java.util.Scanner;
+
+public class Controller {
+
+    private View view;
+    private Model model;
+
+    public Controller(View view, Model model) {
+        this.view = view;
+        this.model = model;
+    }
+
+    public void processUser() {
+        Scanner scanner = new Scanner(System.in);
+
+        TempNotebook tempNotebook = new TempNotebook(model, view, scanner);
+        tempNotebook.inputData();
+
+    }
+}
