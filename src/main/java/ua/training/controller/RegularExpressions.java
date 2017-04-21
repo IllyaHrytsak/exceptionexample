6c6e6e82f2add5fa779310c2f97164cf711e6f2e
Illya Hrytsak <gritsak_web@bigmir.net>
2017-04-21 13:27:43 +0300

    Fixed regex

--- src/main/java/ua/training/controller/RegularExpressions.java
+++ src/main/java/ua/training/controller/RegularExpressions.java
@@ -7,9 +7,9 @@ public interface RegularExpressions {
     String REG_NICKNAME = "^.{1,15}$";
     String REG_HOME_NUMBER = "^\\+38\\(0[\\d]{2}\\)[\\d]{7}$";
     String REG_MOBILE_NUMBER1 = "^\\+38\\(0[\\d]{2}\\)[\\d]{7}$";
-    String REG_MOBILE_NUMBER2 = "^(\\+38\\(0[\\d]{2}\\)[\\d]{7})|next";
-    String REG_EMAIL = "^[a-zA-Z\\d][\\w\\.-]{0,15}@[a-zA-Z]{1,10}\\.(com|ua|ru|net)(\\.ua)?$";
-    String REG_SKYPE = "^[a-zA-Z\\d][\\w\\.-]{0,15}$";
+    String REG_MOBILE_NUMBER2 = "^(\\+38\\(0[\\d]{2}\\)[\\d]{7})|next$";
+    String REG_EMAIL = "^[a-zA-Z\\d][\\w\\.-]{0,15}@[a-zA-Z]{1,10}\\.((com|net|org)(\\.ua)?|ua|ru)$";
+    String REG_SKYPE = "^[a-zA-Z\\d][\\w\\.]{0,20}$";
     String REG_INDEX = "^[\\d]{5}$";
     String REG_TOWN = "^[A-Z]{1}[a-zA-Z]{2,15}$";
     String REG_STREET = "^[A-Z]{1}[a-z]{1,20}(\\s{1}[A-Z]{1}[a-z]{1,20})?$";
