fb3aed0f8bb27a4e17adecc0abff2e0073af1017
IllyaHrytsak <gritsak_web@bigmir.net>
2017-04-21 18:32:15 +0300

    Fixed regex

--- src/main/java/ua/training/controller/RegularExpressions.java
+++ src/main/java/ua/training/controller/RegularExpressions.java
@@ -12,7 +12,7 @@ public interface RegularExpressions {
     String REG_SKYPE = "^[a-zA-Z\\d][\\w\\.]{0,20}$";
     String REG_INDEX = "^[\\d]{5}$";
     String REG_TOWN = "^[A-Z]{1}[a-zA-Z]{2,15}$";
-    String REG_STREET = "^[A-Z]{1}[a-z]{1,20}(\\s{1}[A-Z]{1}[a-z]{1,20})?$";
+    String REG_STREET = "^[A-Z]{1}[a-z]{1,20}$";
     String REG_HOUSE_NUMBER = "^[\\d]{1,3}(\\/[\\d]{1,2})?[a-z]?$";
     String REG_APARTMENT_NUMBER = "^[\\d]{1,3}";
 }
